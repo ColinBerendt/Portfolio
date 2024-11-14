9a2d6cd8bae96d376119f47eb2890736e0269f9a
George Zakhour <geezee@users.noreply.github.com>
2024-11-14 15:01:09 +0100

    Simplified?

--- intermediate/main.js
+++ intermediate/main.js
@@ -1,7 +1,17 @@
-import { getPersonName } from './api/swapi.js';
+async function getPersonName(id) {
+  const url = `https://swapi.dev/api/people/${id}/`;
+  const result = await fetch(url);
+  const person = await result.json();
 
-window.getLukeSkywalker = async () => {
+  return person;
+}
+
+async function getLukeSkywalker() {
   const luke = await getPersonName(1);
   const target = document.getElementById('fetchedData');
   target.innerText = JSON.stringify(luke, null, 2);
-};
+}
+
+document.getElementById("fetch-btn").onclick = function() {
+  getLukeSkywalker();
+}
